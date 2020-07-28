a2001043d51d7403adaabaf210583fb3e36e83e6
Polina Dudchenko <pv.dudchenko@gmail.com>
2020-07-28 17:12:44 +0200

    Test for CachedCharsCounter was changed

--- src/test/java/com/dudchenko/CachedCharsCounterTest.java
+++ src/test/java/com/dudchenko/CachedCharsCounterTest.java
@@ -2,26 +2,28 @@ package com.dudchenko;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.ArgumentMatchers.anyMap;
+
 @ExtendWith(MockitoExtension.class)
 class CachedCharsCounterTest {
 
-    @InjectMocks
-    private CachedCharsCounter сachedCharsCounter;
+    private CachedCharsCounter cachedCharsCounter;
     @Mock
     private CharsCounter charsCounter;
 
     @Test
     public void givenParametersToPut_whenCountCharacters_thenCountCharactersExecuted() {
-        CachedCharsCounter spyCache = Mockito.spy(сachedCharsCounter);
-        spyCache.countCharacters("hi");
-        spyCache.countCharacters("hi");
+        cachedCharsCounter = new CachedCharsCounter(charsCounter);
 
-        Mockito.verify(spyCache, Mockito.times(2)).countCharacters("hi");
-        Mockito.verify(charsCounter, Mockito.times(1)).countCharacters("hi");
+        Mockito.when(charsCounter.countCharacters("hi")).thenReturn(anyMap());
+
+        cachedCharsCounter.countCharacters("hi");
+        cachedCharsCounter.countCharacters("hi");
+
+        Mockito.verify(charsCounter).countCharacters("hi");
     }
 }
