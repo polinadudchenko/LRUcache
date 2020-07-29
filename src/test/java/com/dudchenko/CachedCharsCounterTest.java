d271244f8d948b5aee75287e85e6ef6c1d2ecdfb
Polina Dudchenko <pv.dudchenko@gmail.com>
2020-07-29 10:52:20 +0200

    Rework test for CachedCharsCounter

--- src/test/java/com/dudchenko/CachedCharsCounterTest.java
+++ src/test/java/com/dudchenko/CachedCharsCounterTest.java
@@ -2,28 +2,33 @@ package com.dudchenko;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.mockito.ArgumentMatchers.anyMap;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CachedCharsCounterTest {
 
+    @InjectMocks
     private CachedCharsCounter cachedCharsCounter;
     @Mock
     private CharsCounter charsCounter;
 
     @Test
-    public void givenParametersToPut_whenCountCharacters_thenCountCharactersExecuted() {
-        cachedCharsCounter = new CachedCharsCounter(charsCounter);
-
-        Mockito.when(charsCounter.countCharacters("hi")).thenReturn(anyMap());
+    public void givenStringTwoTimes_whenCountCharacters_thenCharsCounterCountCharactersExecutedOneTime() {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('h', 1);
+        map.put('i', 1);
+        when(charsCounter.countCharacters("hi")).thenReturn(map);
 
         cachedCharsCounter.countCharacters("hi");
         cachedCharsCounter.countCharacters("hi");
 
-        Mockito.verify(charsCounter).countCharacters("hi");
+        verify(charsCounter).countCharacters("hi");
     }
 }
